e11c1007d950a1f2b7a0b16067872fc3d3fb8762
ALTUM-02 <martin249jesus@gmail.com>
2026-06-09 09:51:18 +0300

    new change

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,6 +3,8 @@ from .models import User
 from .serializers import RegisterSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):

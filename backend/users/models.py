bbb18e0f1aaaa0155232e3dae9c9083fc8ca2229
ALTUM-02 <martin249jesus@gmail.com>
2026-06-09 09:14:54 +0300

    new change

--- backend/users/models.py
+++ backend/users/models.py
@@ -13,3 +13,5 @@ class User(AbstractUser):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='tourist')
     
     phone = models.CharField(max_length=20, blank=True, null=True)
+    
+    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)

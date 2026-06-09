75fc43660f29e39c3c5360f9aea818b7fa57a07a
ALTUM-02 <martin249jesus@gmail.com>
2026-06-09 09:52:43 +0300

    new change

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import User
 from .serializers import RegisterSerializer
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 

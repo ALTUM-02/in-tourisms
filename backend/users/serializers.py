faab0faad16008781cb6f3e0be2446a02517c66a
ALTUM-02 <martin249jesus@gmail.com>
2026-06-09 09:32:27 +0300

    new hange

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,2 +1,21 @@
 from rest_framework import serializers
 from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role', 'phone', 'profile_image']
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=validated_data.get('role', 'tourist'),
+            phone=validated_data.get('phone', ''),
+            profile_image=validated_data.get('profile_image', None)
+        )
+        return user

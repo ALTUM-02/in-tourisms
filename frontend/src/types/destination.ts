1071fc3865802f2c18ca47401eb1bdec982ba660
ALTUM-02 <martin249jesus@gmail.com>
2026-06-11 02:28:44 +0300

    new change

--- frontend/src/types/destination.ts
+++
@@ -1,9 +0,0 @@
-export interface Destination {
-  id: number;
-  name: string;
-  region: string;
-  district: string;
-  description: string;
-  location: string;
-  entry_fee: string;
-  image: string

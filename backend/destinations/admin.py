b3600d9d686f0f2c8cb8fa7b567bb5dd6d81f1ed
ALTUM-02 <martin249jesus@gmail.com>
2026-06-10 10:40:59 +0300

    new change

--- backend/destinations/admin.py
+++ backend/destinations/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from djan
+from .models import Destination
+
+
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'region',
+        'entry_fee',
+        'created_at'
+    )
+
+    search_fields = (
+        'name',
+        'region'
+    )
+
+    list_filter = (
+        'region',
+    )

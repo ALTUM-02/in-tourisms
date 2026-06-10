da2e219b46d2408f29f76c9bc4b8afd918c575a4
ALTUM-02 <martin249jesus@gmail.com>
2026-06-10 10:39:45 +0300

    new change

--- backend/destinations/models.py
+++ backend/destinations/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Destination(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    region = models.CharField(max_length=100)
+
+    district = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    description = models.TextField()
+
+    location = models.CharField(max_length=255)
+
+    entry_fee = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    image = models.ImageField(
+        upload_to='destinations/'
+    )
+
+    video = models.FileField(
+        upload_to='destination_videos/',
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.name

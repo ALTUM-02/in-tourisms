1a7a82205943ce0e23db64143bb7c94f0eb3ed98
ALTUM-02 <martin249jesus@gmail.com>
2026-06-14 02:00:43 +0300

    new change

--- backend/wildlife/views.py
+++ backend/wildlife/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Animal
+from .serializers import AnimalSerializer
+
+
+class AnimalViewSet(
+    viewsets.ModelViewSet
+):
+
+    queryset = Animal.objects.all()
+
+    serializer_class = AnimalSerializer

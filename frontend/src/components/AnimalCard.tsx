ef795f221121a3ca3863963090a6a9dd46ba9f88
ALTUM-02 <martin249jesus@gmail.com>
2026-06-14 15:17:30 +0300

    new change

---
+++ frontend/src/components/AnimalCard.tsx
@@ -0,0 +1,33 @@
+import { Animal } from "../types/animal";
+
+interface Props {
+  animal: Animal;
+}
+
+function AnimalCard({ animal }: Props) {
+  return (
+    <div className="bg-white rounded-xl shadow">
+
+      <img
+        src={animal.image}
+        alt={animal.name}
+        className="w-full h-60 object-cover"
+      />
+
+      <div className="p-4">
+
+        <h2 className="font-bold text-xl">
+          {animal.name}
+        </h2>
+
+        <p>
+          {animal.destination_name}
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default AnimalCard;

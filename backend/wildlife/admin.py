513d5c4e72b726e2e8814953262db55e0da78baa
ALTUM-02 <martin249jesus@gmail.com>
2026-06-14 13:38:41 +0300

    new chnage

--- backend/wildlife/admin.py
+++ backend/wildlife/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Animal
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'destination'
+    )
+
+    search_fields = (
+        'name',
+        'scientific_name'
+    )
--- backend/wildlife/models.py
+++ backend/wildlife/models.py
@@ -33,3 +33,19 @@ class Animal(models.Model):
 
     def __str__(self):
         return self.name
+    
+from django.db import models
+from destinations.models import Destination
+
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    destination = models.ForeignKey(
+        Destination,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name

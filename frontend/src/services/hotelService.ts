9cbba292d5d6d782b11cf08df4c1414bd5fcb0ec
ALTUM-02 <martin249jesus@gmail.com>
2026-06-15 19:04:02 +0300

    new change

---
+++ frontend/src/services/hotelService.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API =
+  "http://127.0.0.1:8000/api";
+
+export const getHotels = () => {
+  return axios.get(
+    `${API}/hotels/`
+  );
+};
---
+++ frontend/src/types/hotel.ts
@@ -0,0 +1,9 @@
+export interface Hotel {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  price_per_night: string;
+  stars: number;
+  destination_name: string;
+}

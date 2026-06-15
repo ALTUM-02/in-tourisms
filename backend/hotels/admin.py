50c838f4012f5eb2ab98a199e0dc364150c46019
ALTUM-02 <martin249jesus@gmail.com>
2026-06-15 17:01:29 +0300

    new change

--- backend/hotels/admin.py
+++ backend/hotels/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Hotel
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'destination',
+        'price_per_night',
+        'stars'
+    )
+
+    list_filter = (
+        'stars',
+        'destination'
+    )
+
+    search_fields = (
+        'name',
+    )

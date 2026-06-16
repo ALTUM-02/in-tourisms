a7fd11d63e75de4dc880c2b186db78f46fda2e94
ALTUM-02 <martin249jesus@gmail.com>
2026-06-16 13:50:53 +0300

    new change

--- backend/foods/admin.py
+++ backend/foods/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Food
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'destination',
+        'category',
+        'price'
+    )
+
+    list_filter = (
+        'category',
+        'destination'
+    )

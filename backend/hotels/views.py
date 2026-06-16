29705eab3eb6ff5f482c0e2e54df1e79845b74e0
ALTUM-02 <martin249jesus@gmail.com>
2026-06-16 13:01:15 +0300

    new change

--- backend/hotels/views.py
+++ backend/hotels/views.py
@@ -7,10 +7,23 @@ from .models import Hotel
 from .serializers import HotelSerializer
 
 
-class HotelViewSet(
-    viewsets.ModelViewSet
-):
-
+class HotelViewSet(viewsets.ModelViewSet):
     queryset = Hotel.objects.all()
-
     serializer_class = HotelSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if not serializer.is_valid():
+            print(serializer.errors)
+            return Response(
+                serializer.errors,
+                status=400
+            )
+
+        serializer.save()
+
+        return Response(
+            serializer.data,
+            status=201
+        )

1bf06df53aa239138a7f1b273087f728f8106d75
ALTUM-02 <martin249jesus@gmail.com>
2026-06-16 13:23:03 +0300

    new change

--- backend/hotels/views.py
+++ backend/hotels/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .models import Hotel
 from .serializers import HotelSerializer
@@ -15,15 +16,16 @@ class HotelViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
 
         if not serializer.is_valid():
-            print(serializer.errors)
+            print("ERRORS:", serializer.errors)
+
             return Response(
                 serializer.errors,
-                status=400
+                status=status.HTTP_400_BAD_REQUEST
             )
 
         serializer.save()
 
         return Response(
             serializer.data,
-            status=201
+            status=status.HTTP_201_CREATED
         )

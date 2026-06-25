3f47944476c587168152dfe12d76688cbab87e1c
ALTUM-02 <martin249jesus@gmail.com>
2026-06-25 06:34:05 +0300

    new change

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,43 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div>
-      <h1>Welcome to Tanzania Tourism</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-100">
+
+      <h1 className="text-5xl font-bold mb-6">
+        Welcome to Tanzania Tourism
+      </h1>
+
+      <div className="flex gap-4">
+
+        <Link
+          to="/login"
+          className="
+          bg-emerald-600
+          text-white
+          px-6
+          py-3
+          rounded-lg
+          "
+        >
+          Login
+        </Link>
+
+        <Link
+          to="/register"
+          className="
+          bg-blue-600
+          text-white
+          px-6
+          py-3
+          rounded-lg
+          "
+        >
+          Register
+        </Link>
+
+      </div>
+
     </div>
   );
 }

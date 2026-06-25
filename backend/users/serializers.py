ccce2300f7bc154da0aa3f88c83ac644f8fc1991
ALTUM-02 <martin249jesus@gmail.com>
2026-06-25 23:41:39 +0300

    new change

---
+++ backend/users/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
+
+    class Meta:
+        model = User
+
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "phone",
+            "role",
+            "full_name",
+            "country",
+            "profile_photo",
+        ]
+
+    def create(self, validated_data):
+
+        return User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            phone=validated_data.get("phone", ""),
+            role=validated_data.get("role", "tourist"),
+            full_name=validated_data.get("full_name", ""),
+            country=validated_data.get("country", ""),
+        )

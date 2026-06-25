7599619a102ecf589689d1dbad35f5784e6f3dd2
ALTUM-02 <martin249jesus@gmail.com>
2026-06-25 06:31:09 +0300

    new change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,29 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Destinations from "./pages/Destination";
-import Wildlife from "./pages/wildlife";
-import Hotels from "./pages/Hotels";
-import Foods from "./pages/Foods";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Routes>
 
-      <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
 
-        <Route
-          path="/"
-          element={<Home />}
-        />
+      <Route
+        path="/login"
+        element={<Login />}
+      />
 
-        <Route
-          path="/destinations"
-          element={<Destinations />}
-        />
+      <Route
+        path="/register"
+        element={<Register />}
+      />
 
-        <Route
-          path="/wildlife"
-          element={<Wildlife />}
-        />
-
-        <Route
-          path="/hotels"
-          element={<Hotels />}
-        />
-
-        <Route
-          path="/foods"
-          element={<Foods />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
+    </Routes>
   );
 }
 

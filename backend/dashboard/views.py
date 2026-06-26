a848cf7f9eabb1dd019a47567f9023c623fb9f8e
ALTUM-02 <martin249jesus@gmail.com>
2026-06-27 01:56:48 +0300

    new changge

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from destinations.models import Destination
+from wildlife.models import Animal
+from hotels.models import Hotel
+from foods.models import Food
+
+class DashboardView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "destinations": Destination.objects.count(),
+            "animals": Animal.objects.count(),
+            "hotels": Hotel.objects.count(),
+            "foods": Food.objects.count(),
+        })

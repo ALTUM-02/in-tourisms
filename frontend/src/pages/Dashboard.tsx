c4332dca5a5992e0d8e494bf079ac8b1e4145087
ALTUM-02 <martin249jesus@gmail.com>
2026-06-27 02:11:22 +0300

    new changge

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import DashboardLayout
-from "../layouts/DashboardLayout";
+from "../components/dashboard/DashboardLayout";
 
 import Hero
 from "../components/dashboard/Hero";

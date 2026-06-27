286426b45c2f11dc64a79a419c955bbfd4cedfb5
ALTUM-02 <martin249jesus@gmail.com>
2026-06-27 11:13:20 +0300

    new change

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -6,4 +6,4 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from destinations.models import Destination
-from wildlife.models import Animal
+from wildlife.models 

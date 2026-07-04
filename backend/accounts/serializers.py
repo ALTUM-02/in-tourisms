8fc53ce8a3db0b913cabef9e389fc2d112e4216a
ALTUM-02 <martin249jesus@gmail.com>
2026-07-04 05:11:48 +0300

    new change

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'role', 'phone', 'avatar', 'date_joined']
+        read_only_fields = ['id', 'date_joined']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password_confirm', 'first_name', 'last_name', 'phone', 'role']
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError({'password_confirm': 'Passwords do not match.'})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        role = validated_data.pop('role', 'tourist')
+        user = User.objects.create_user(
+            username=validated_data['email'],
+            **validated_data
+        )
+        user.role = role
+        user.save()
+        return user
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True, min_length=8)
+
+    def validate_old_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError('Old password is incorrect.')
+        return value

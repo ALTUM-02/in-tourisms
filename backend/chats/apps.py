ae9679183cdf21aaee92a75d349babe7a5caf1e7
ALTUM-02 <martin249jesus@gmail.com>
2026-07-10 13:53:52 +0300

    new change

--- backend/chats/apps.py
+++ backend/chats/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-
 class ChatsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'chats'
+    label = 'my_unique_chats' # Add this line to resolve the conflict

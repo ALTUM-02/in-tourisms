eeb6e388f7a06efe54840631dd0820f63563be04
ALTUM-02 <martin249jesus@gmail.com>
2026-07-10 13:46:36 +0300

    new change

--- backend/bookings/admin.py
+++ backend/bookings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Booking
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'hotel', 'check_in', 'check_out', 'status', 'total_price']
+    list_filter = ['status']

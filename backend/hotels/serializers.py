a623a07c38fc1c58b21e048fdc58c9c9b72fc611
ALTUM-02 <martin249jesus@gmail.com>
2026-07-14 00:37:05 +0300

    new change

--- backend/hotels/serializers.py
+++ backend/hotels/serializers.py
@@ -1,16 +1,27 @@
 from rest_framework import serializers
-from .models import Hotel
+from .models import Hotel, RoomType
 
 
-class HotelSerializer(
-    serializers.ModelSerializer
-):
+class RoomTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoomType
+        fields = '__all__'
 
-    destination_name = serializers.CharField(
-        source='destination.name',
-        read_only=True
-    )
+
+class HotelSerializer(serializers.ModelSerializer):
+    room_types = RoomTypeSerializer(many=True, read_only=True)
+    destination_name = serializers.CharField(source='destination.name', read_only=True)
 
     class Meta:
         model = Hotel
         fields = '__all__'
+        read_only_fields = ['id', 'created_at']
+
+
+class HotelListSerializer(serializers.ModelSerializer):
+    destination_name = serializers.CharField(source='destination.name', read_only=True)
+
+    class Meta:
+        model = Hotel
+        fields = ['id', 'name', 'description', 'location', 'image', 'rating',
+                  'price_per_night', 'destination', 'destination_name', 'amenities']

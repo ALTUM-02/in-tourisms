57aa64bfe602326349ab311099ce95fb6c145fa2
ALTUM-02 <martin249jesus@gmail.com>
2026-07-13 23:31:12 +0300

    new change

--- backend/favorites/admin.py
+++ backend/favorites/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Favorite
 
-# Register your models here.
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ['user', 'destination', 'created_at']

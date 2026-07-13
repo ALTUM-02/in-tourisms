ea9a28ca6219ec12b2e3934ab4c22dbe3eb2651b
ALTUM-02 <martin249jesus@gmail.com>
2026-07-14 00:09:10 +0300

    new change

--- backend/foods/views.py
+++ backend/foods/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Restaurant, MenuItem
+from .serializers import RestaurantSerializer, RestaurantListSerializer, MenuItemSerializer
+from destinations.views import IsOfficerOrReadOnly
 
-# Create your views here.
-from rest_framework import viewsets
 
-from .models import Food
-from .serializers import FoodSerializer
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.select_related('destination').all()
+    permission_classes = [IsOfficerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['destination', 'cuisine_type']
+    search_fields = ['name', 'cuisine_type']
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return RestaurantListSerializer
+        return RestaurantSerializer
+
+    @action(detail=True, methods=['get'])
+    def menu(self, request, pk=None):
+        restaurant = self.get_object()
+        items = restaurant.menu_items.all()
+        serializer = MenuItemSerializer(items, many=True)
+        return Response(serializer.data)
 
 
-class FoodViewSet(
-    viewsets.ModelViewSet
-):
-
-    queryset = Food.objects.all()
-
-    serializer_class = FoodSerializer
+class MenuItemViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.select_related('restaurant').all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [IsOfficerOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['restaurant', 'category', 'is_vegetarian', 'is_vegan', 'is_halal']

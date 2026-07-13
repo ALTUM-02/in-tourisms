cf6d08c86a148379f5419b960f1a33b5e6026a7b
ALTUM-02 <martin249jesus@gmail.com>
2026-07-13 23:29:33 +0300

    new change

--- backend/destinations/serializers.py
+++ backend/destinations/serializers.py
@@ -1,20 +1,17 @@
 from rest_framework import serializers
 from .models import Destination
 
+
 class DestinationSerializer(serializers.ModelSerializer):
-
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = Destination
-        fields = "__all__"
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
-    def get_image(self,obj):
 
-        request = self.context.get("request")
-
-        if obj.image:
-
-            return request.build_absolute_uri(obj.image.url)
-
-        return None
+class DestinationListSerializer(serializers.ModelSerializer):
+    """Lighter serializer for list views."""
+    class Meta:
+        model = Destination
+        fields = ['id', 'name', 'description', 'location', 'country', 'image',
+                  'rating', 'review_count', 'category', 'is_featured', 'best_season', 'entry_fee']

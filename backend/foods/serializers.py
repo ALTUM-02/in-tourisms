f4bc57ce76ce71f76a12b10c3b9f045efdb5dc6a
ALTUM-02 <martin249jesus@gmail.com>
2026-07-14 00:08:04 +0300

    new change

--- backend/foods/serializers.py
+++ backend/foods/serializers.py
@@ -1,16 +1,30 @@
 from rest_framework import serializers
-from .models import Food
+from .models import Restaurant, MenuItem
 
 
-class FoodSerializer(
-    serializers.ModelSerializer
-):
-
-    destination_name = serializers.CharField(
-        source='destination.name',
-        read_only=True
-    )
+class MenuItemSerializer(serializers.ModelSerializer):
+    restaurant_name = serializers.CharField(source='restaurant.name', read_only=True)
 
     class Meta:
-        model = Food
+        model = MenuItem
         fields = '__all__'
+        read_only_fields = ['id']
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    menu_items = MenuItemSerializer(many=True, read_only=True)
+    destination_name = serializers.CharField(source='destination.name', read_only=True)
+
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at']
+
+
+class RestaurantListSerializer(serializers.ModelSerializer):
+    destination_name = serializers.CharField(source='destination.name', read_only=True)
+
+    class Meta:
+        model = Restaurant
+        fields = ['id', 'name', 'description', 'image', 'cuisine_type',
+                  'rating', 'price_range', 'destination', 'destination_name']

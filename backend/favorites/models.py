1568e976a42e682507f2aaaaf745ae09206c1d71
ALTUM-02 <martin249jesus@gmail.com>
2026-07-14 01:45:15 +0300

    new change

--- backend/favorites/models.py
+++ backend/favorites/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.conf import settings
-from favorites.models import Favorite
+from destinations.models import Destination
+
 
 class Favorite(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='favorites')
-    destination = models.ForeignKey('destinations.Destination', on_delete=models.CASCADE, related_name='favorited_by')
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='favorited_by')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
